b5db63892140d487dc6ccd56fcd33afe99ddd62d
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-31 12:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 function createTable() {
-//Write your code here
 	let table = document.getElementById("myTable");
-    let rows = parseInt(prompt("Input number of rows"));
-    let cols = parseInt(prompt("Input number of columns"));
-	if (isNaN(rows) || isNaN(cols)) {
-        alert("Please enter valid numbers.");
-        return;
-    }
+	let rows = parseInt(prompt("Input number of rows"));
+	let cols = parseInt(prompt("Input number of columns"));
 
-    if (rows <= 0 || cols <= 0) {
-        alert("Please enter positive numbers.");
-        return;
-    }
-	table.innerHTML="";
-	for(let i=0;i<rows;i++){
-		let curr=document.createElement("tr");
-		
-		for(let j=0;j<cols;j++){
-			let data;
-			if(i === 0){
-				data=document.createElement("th");
-			}
-			else{
-				
-				data=document.createElement("td");
-			}
-			data.innerHTML=`Row-${i} Column-${j}`;
-			curr.appendChild(data);
-		}
-			table.appendChild(curr);
+	if (isNaN(rows) || isNaN(cols)) {
+		alert("Please enter valid numbers.");
+		return;
 	}
 
+	if (rows <= 0 || cols <= 0) {
+		alert("Please enter positive numbers.");
+		return;
+	}
+
+	table.innerHTML = "";
+
+	for (let i = 0; i < rows; i++) {
+		let curr = document.createElement("tr");
+
+		for (let j = 0; j < cols; j++) {
+			let data;
+			if (i === 0) {
+				data = document.createElement("th");
+			} else {
+				data = document.createElement("td");
+			}
+			data.innerHTML = `Row-${i} Column-${j}`;
+			curr.appendChild(data);
+		}
+		table.appendChild(curr);
+	}
 }
