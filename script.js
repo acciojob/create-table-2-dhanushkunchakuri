92e12da482ced215197c60024172f40173dcedb3
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-31 12:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function createTable() {
 	let table = document.getElementById("myTable");
+
 	let rows = parseInt(prompt("Input number of rows"));
 	let cols = parseInt(prompt("Input number of columns"));
 
@@ -16,18 +17,12 @@ function createTable() {
 	table.innerHTML = "";
 
 	for (let i = 0; i < rows; i++) {
-		let curr = document.createElement("tr");
-
+		let tr = document.createElement("tr");
 		for (let j = 0; j < cols; j++) {
-			let data;
-			if (i === 0) {
-				data = document.createElement("th");
-			} else {
-				data = document.createElement("td");
-			}
-			data.innerHTML = `Row-${i} Column-${j}`;
-			curr.appendChild(data);
+			let td = document.createElement("td");
+			td.innerHTML = `Row-${i} Column-${j}`;
+			tr.appendChild(td);
 		}
-		table.appendChild(curr);
+		table.appendChild(tr);
 	}
 }
