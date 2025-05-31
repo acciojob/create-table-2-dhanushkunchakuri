1584fe76d882e2d61ba80550a87a2b9e2bdc0197
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-31 12:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createTable() {
 //Write your code here
-	let table = document.getElementById("table");
+	let table = document.getElementById("myTable");
     let rows = parseInt(prompt("Input number of rows"));
     let cols = parseInt(prompt("Input number of columns"));
 	if (isNaN(rows) || isNaN(cols)) {
